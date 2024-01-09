3a277eac9cd5bbdc0ad9cacb7a0f8a7bae0c75a4
ValerioTorto <valerio.tortorelli@gmail.com>
2024-01-09 17:32:54 +0100

    Devo far funzionare /show

--- src/main/java/org/learning/springlamiapizzeriacrud/repository/PizzaRepository.java
+++ src/main/java/org/learning/springlamiapizzeriacrud/repository/PizzaRepository.java
@@ -5,8 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PizzaRepository { extends JpaRepository<Pizza, Integer>{
-    }
+public interface PizzaRepository extends JpaRepository<Pizza, Integer>{
 }
 
 
