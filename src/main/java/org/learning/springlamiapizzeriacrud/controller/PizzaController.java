031906cfd4cc21c6559d2fda4975dd5d98ffc892
ValerioTorto <valerio.tortorelli@gmail.com>
2024-01-09 11:15:04 +0100

    Setup

---
+++ src/main/java/org/learning/springlamiapizzeriacrud/controller/PizzaController.java
@@ -0,0 +1,4 @@
+package org.learning.springlamiapizzeriacrud.controller;
+
+public class PizzaController {
+}
---
+++ src/main/java/org/learning/springlamiapizzeriacrud/repository/PizzaRepository.java
@@ -0,0 +1,4 @@
+package org.learning.springlamiapizzeriacrud.repository;
+
+public interface PizzaRepository {
+}
