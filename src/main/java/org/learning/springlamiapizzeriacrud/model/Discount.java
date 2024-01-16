3b0fe9957d8d6fc7ab9aaff05c0965daaeb2e2b4
ValerioTorto <valerio.tortorelli@gmail.com>
2024-01-16 15:37:30 +0100

    prova push

---
+++ src/main/java/org/learning/springlamiapizzeriacrud/model/Discount.java
@@ -0,0 +1,62 @@
+package org.learning.springlamiapizzeriacrud.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+@Entity
+@Table(name = "discounts")
+public class Discount {
+    //atrributi
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private LocalDate startDate;
+    @Column(nullable = false)
+    private LocalDate expireDate;
+    private String note;
+    @ManyToOne
+    private Pizza pizza;
+
+    //GETTER E SETTER
+
+    public Pizza getPizza() {
+        return pizza;
+    }
+
+    public void setPizza(Pizza pizza) {
+        this.pizza = pizza;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getExpireDate() {
+        return expireDate;
+    }
+
+    public void setExpireDate(LocalDate expireDate) {
+        this.expireDate = expireDate;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
